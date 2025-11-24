30dd849f90255d8652bd55d9d99fe91c02c69f47
Eishaan-Khatri <eishaankhatri@gmail.com>
2025-11-24 06:24:48 +0530

    test deploy

--- api/main.py
+++ api/main.py
@@ -1,5 +1,7 @@
-import os
-import sys
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
 import json
 import logging
 from datetime import datetime, timezone
